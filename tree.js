46fd40b93ea895252119ccc77a86fb22c7cf1c9b
andyphancode <andyphanpxh@gmail.com>
2023-09-02 18:04:28 -0700

    Update tree.js

--- tree.js
+++ tree.js
@@ -15,20 +15,77 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let ourStack = [this.root];
+
+    let sum = 0;
+
+    while (ourStack.length) {
+
+      let current = ourStack.pop()
+
+      if (current) {
+        sum += current.val 
+        if (current.children) {
+          for (let child of current.children) {
+            ourStack.push(child);
+          }
+        }
+      }
+
+    }
+
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let ourStack = [this.root];
 
+    let sum = 0;
+
+    while (ourStack.length) {
+
+      let current = ourStack.pop()
+
+      if (current) {
+        if(current.val % 2 === 0) {sum += 1} 
+        if (current.children) {
+          for (let child of current.children) {
+            ourStack.push(child);
+          }
+        }
+      }
+
+    }
+
+    return sum;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let ourStack = [this.root];
 
+    let sum = 0;
+
+    while (ourStack.length) {
+
+      let current = ourStack.pop()
+
+      if (current) {
+        if(current.val > lowerBound) {sum += 1} 
+        if (current.children) {
+          for (let child of current.children) {
+            ourStack.push(child);
+          }
+        }
+      }
+
+    }
+
+    return sum;
   }
 }
 
